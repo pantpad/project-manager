f4752b65593f96b5480798360d7f166dbb887a78
pantpad <138708449+pantpad@users.noreply.github.com>
2024-02-08 15:24:08 +0100

    Initialize project skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Ciao</h1>
+      <nav>
+        <h2>YOUR PROJECTS</h2>
+        <button>+ Add Project</button>
+        <h3>My project/s</h3>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </nav>
+      <main>
+        <img src="" alt="" width="72" height="72" />
+        <h2>No Project Selected</h2>
+        <p>Select a project or get started with a new one</p>
+        <button> Create new project</button>
+      </main>
     </>
   );
 }
