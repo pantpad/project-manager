7b5d0bf43d74828e4cecadc5adf8ff55cfa6616e
pantpad <138708449+pantpad@users.noreply.github.com>
2024-02-09 17:51:24 +0100

    Add dialog

---
+++ src/components/ui/Modal/Modal.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+import Button from "../Button/Button";
+
+import styles from "./Modal.module.css";
+import { styleClsx } from "../../../utils/styleComponent";
+
+export default forwardRef(function Modal({ className, ...props }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+  return createPortal(
+    <dialog className={styleClsx(styles, className)} ref={dialog} {...props}>
+      <h1>Ciao</h1>
+      <form method="dialog">
+        <Button variant="primary" size="medium">
+          Okay
+        </Button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+  );
+});
